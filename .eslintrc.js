ac79c04134298f6fa768b32f87ac59c5f8f7134c
tadasdulke <tadasdulke5@gmail.com>
2023-11-21 22:05:48 +0200

    feat: add eslint/prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'standard-with-typescript',
+    'plugin:import/recommended',
+    'plugin:promise/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
